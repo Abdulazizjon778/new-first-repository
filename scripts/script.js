018a63b9181c72a0b06bbe7f8c940b054687b67c
Abdulazizjon778 <abdulazizjonazizov778>
2023-02-23 16:39:36 +0500

    third commit

---
+++ scripts/script.js
@@ -0,0 +1,29 @@
+// function perimetr(a , b , c ) {
+//     return a+b+c;
+// }
+
+// let num1 = +prompt("Напишите сторону А")
+// let num2 = +prompt("Напишите сторону B")
+// let num3 = +prompt("Напишите сторону C")
+
+// alert(`Периметр треугольника `  +  perimetr(num1 , num2, num3)+   ` sm`)
+
+
+// function num(a) {
+//     let r = +prompt("Напишите число")
+//     if (r%2==0) {
+//         alert("четное")
+//     }else{
+//         alert("нечетное")
+//     }
+// }
+// num()
+
+// function radius(a , b) {
+//     let d = +prompt("Введите радиус")
+//     let y = (d*2)* parseInt(Math.PI)
+//     if (y) {
+//         alert("Oкружность " + y +  ` sm`)
+//     }
+// }
+// radius()
